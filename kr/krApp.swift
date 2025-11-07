6c3901d55245fa016cf4031a6af1d78980d33c01
VitaliiZhu <23zhuk.v@nltu.lviv.ua>
2025-11-07 09:14:17 +0200

    Initial Commit

---
+++ kr/krApp.swift
@@ -0,0 +1,17 @@
+//
+//  krApp.swift
+//  kr
+//
+//  Created by ІПЗ-31/1 on 07.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct krApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
